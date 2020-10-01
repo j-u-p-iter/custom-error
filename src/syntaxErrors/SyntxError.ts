e94191a7b4ac4301b834e149c473d75d8ba02a3b
j.u.p.iter <pn79122821224@gmail.com>
2020-10-01 10:51:07 +0500

    feat(syntax error): adds custom syntax error

---
+++ src/syntaxErrors/SyntxError.ts
@@ -0,0 +1,22 @@
+import { BaseErrorConfig, CustomError } from "../CustomError";
+
+/**
+ * ValidationError knows about
+ * all properties child objects have.
+ */
+interface SyntaxErrorConfig extends BaseErrorConfig {
+  invalidJSON?: string;
+}
+
+/**
+ * We name it SyntxError, cause there's already
+ * built in SyntaxError class
+ *
+ */
+export class SyntxError extends CustomError {
+  constructor(message: string, config: SyntaxErrorConfig = {}) {
+    super(message, config);
+
+    this.name = "SyntaxError";
+  }
+}
